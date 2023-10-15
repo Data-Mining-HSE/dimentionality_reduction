b62eaefa2b19d6fe8260bd266cb2fdbc58ea2462
Trutnev Aleksei <alexgiving@mail.ru>
2023-10-15 23:09:29 +0300

    fix size

--- src/visualize.py
+++ src/visualize.py
@@ -1,20 +1,29 @@
 
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
+
+def get_subplots(ncols: int, figsize: tuple[int, int]) -> tuple[Figure, Axes]:
+    figure, axes = plt.subplots(ncols=ncols, nrows=1)
+
+    figure.set_figheight(figsize[0])
+    figure.set_figwidth(figsize[1])
+
+    figure.tight_layout()
+    return figure, axes
 
 
 def get_histograms(dataset: pd.DataFrame) -> None:
-    dataset.hist(figsize=(9, 7))
+    _, axes = get_subplots(len(dataset.columns), figsize=(3, 10))
+
+    for column_id, column in enumerate(dataset.columns):
+        pd.DataFrame(dataset[column]).hist(ax=axes[column_id])
 
 
 def get_boxplot(dataset: pd.DataFrame) -> None:
-    figure, axes = plt.subplots(ncols=len(dataset.columns), nrows=1)
-
-    figure.set_figheight(3)
-    figure.set_figwidth(8)
+    _, axes = get_subplots(len(dataset.columns), figsize=(5, 9))
 
     for column_id, column in enumerate(dataset.columns):
         pd.DataFrame(dataset[column]).boxplot(ax=axes[column_id])
-
-    figure.tight_layout()
- 
