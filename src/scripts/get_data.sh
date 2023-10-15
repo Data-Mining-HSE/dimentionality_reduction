2ca91c9eb217226f7a8f540f8cadd73c010a87a5
alexgiving <alexgiving@mail.ru>
2023-10-15 14:00:03 +0300

    Initial data loader

--- src/scripts/get_data.sh
+++ src/scripts/get_data.sh
@@ -1,10 +1,12 @@
 #!bin/bash
 
-mkdir -p data
-pushd data
+mkdir -p temp
+
+
+pushd temp
 wget https://github.com/Data-Mining-HSE/data_directory/archive/refs/heads/main.zip .
 unzip main.zip
 unzip data_directory-main/dimentionality_reduction.zip
-mv __MACOSX/dimentionality_reduction dimentionality_reduction 
-ls | grep -v dimentionality_reduction | xargs rm -rfv
+mv dimentionality_reduction ../data
 popd
+rm -rf temp
